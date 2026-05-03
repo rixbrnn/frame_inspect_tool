49bd8bae554737debc82bee7464824bf87ebdfce
Richard Bringmann <richardbringmann160301@gmail.com>
2026-05-03 13:06:05 -0300

    Remove unneeded script

--- download_data.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="rixbrnn/frame_inspect_tool_data",
-    repo_type="dataset",
-    local_dir="./recordings",
-    local_dir_use_symlinks=False
-)
